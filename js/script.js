fcf516c7a64070613d40ddb02b7a07d8bb4f6865
MariuszPE <mariusz.pela1@gmail.com>
2018-07-17 16:03:35 +0200

    functionality of inputs added (end date don't work)

--- js/script.js
+++ js/script.js
@@ -1,23 +1,59 @@
 window.onload = function () {
+    document.getElementById('currencyName').value = '';
+    document.getElementById('currencyStartDate').value = '';
+
+
     document.querySelector("#submitButton").addEventListener("click", function () {
-        const currencyName = document.querySelector("#currencyName").value;
-        const currencyInformationType = document.querySelector("#currencyInformationType").value;
-        const currencyDate = document.querySelector("#currencyDate").value;
-        console.log(currencyName, currencyInformationType, currencyDate);
+        let currencyName = document.querySelector("#currencyName").value;
+        let currencyInformationType = document.querySelector("#currencyInformationType").value;
+        let currencyStartDate = document.querySelector("#currencyStartDate").value;
+        let currencyEndDate = document.querySelector("#currencyEndDate").value;
+        console.log(currencyName, currencyInformationType, currencyStartDate, currencyEndDate);
 
+        let ciType = bidAskAverage(currencyInformationType);
 
-        var xmlhttp = new XMLHttpRequest();
-        var url = 'http://api.nbp.pl/api/exchangerates/rates/a/eur';
+        let xmlhttp = new XMLHttpRequest();
+        let url = 'http://api.nbp.pl/api/exchangerates/rates/' + ciType + '/' + currencyName +
+        '/' + currencyStartDate;
+        let cR = currRepresentation(currencyInformationType);
 
         xmlhttp.onreadystatechange = function () {
             if (this.readyState == 4 && this.status == 200) {
-                var myArr = JSON.parse(this.responseText);
-                document.getElementById("id01").innerHTML = "Date: " +
-                    myArr.rates[0].effectiveDate + "<br>" + "Rate: " + myArr.rates[0].mid;
+                let myArr = JSON.parse(this.responseText);
+                document.getElementById("answer").innerHTML = "<strong>Date: </strong>" +
+                    myArr.rates[0].effectiveDate + "<br>" + "<strong>Rate: </strong>" + 
+                    myArr.rates[0][cR];
             }
         };
         xmlhttp.open("GET", url, true);
         xmlhttp.send();
 
     });
+
+    function bidAskAverage(currencyInformationType) {
+        let cit = currencyInformationType;
+        if(cit === 'Bid' || cit === 'Ask') {
+            return 'c';
+        } else {
+            return 'a';
+        }
+    }
+
+
+    function currRepresentation(currencyInformationType) {
+        let cit = currencyInformationType;
+        if(cit === 'Bid') {
+            return 'bid';
+        } else if (cit === 'Ask') {
+            return 'ask';
+        } else {
+            return 'mid';
+        }
+    }
+
+    
+
+
+
+
 }
